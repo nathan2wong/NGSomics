eb8d49c7c7e8660ef192746f3dfee4b4361d5857
Nathan Wong <nathan2wong@gmail.com>
2016-02-17 15:23:43 -0800

    backup

--- samtools.sh
+++ samtools.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 #Requires input of "aligned.sam" and "reference.fna"
-samtools view -b -S -o aligned.bam aligned.sam #Converting SAM to BAM
-samtools sort aligned.bam aligned.sorted #Sort BAM file
+./samtools view -b -S -o aligned.bam aligned.sam #Converting SAM to BAM
+echo "111"
+./samtools sort aligned.bam aligned.sorted #Sort BAM file
 #Variant calling 
-samtools mpileup -g -f reference.fna aligned.bam > variants.bcf & 
+echo "222"
+./samtools mpileup -g -f reference.fna aligned.bam > variants.bcf & 
 java -jar VarScan.v2.3.7.jar mpileup2snp -g -f reference.fna varscandata/genomes/sim_reads_aligned.sorted.bam > sim_variants.bcf
 
-bcftools call -c -v variants.bcf > variants.vcf #BCF to VCF File
-samtools tview variants.vcf #view output
+echo "333"
+
+#bcftools call -c -v variants.bcf > variants.vcf #BCF to VCF File
+#./samtools tview variants.vcf #view output
