ecbe546efe4cfeef5eb50c3483a637694a3e0a23
Nathan Wong <nathan2wong@gmail.com>
2016-02-17 14:39:33 -0800

    updated entrypoint script

--- auto.sh
+++ auto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-samtools index example.bam &
-samtools tview example.bam -a 
-
-gatk 
+samtools view -b -S -o aligned.bam aligned.sam #Converting SAM to BAM
+samtools sort aligned.bam aligned.sorted #Sort BAM file
+samtools mpileup -g -f reference.fna aligned.bam > variants.bcf #Variant calling
+bcftools call -c -v variants.bcf > variants.vcf #BCF to VCF File
+samtools tview variants.vcf #view output
