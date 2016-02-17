bd83e014cecb710f56ad6e531bbcf8797b12960e
Nathan Wong <nathan2wong@gmail.com>
2016-02-17 14:41:32 -0800

    comments

--- auto.sh
+++ auto.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Requires input of "aligned.bam" and "reference.fna"
 samtools view -b -S -o aligned.bam aligned.sam #Converting SAM to BAM
 samtools sort aligned.bam aligned.sorted #Sort BAM file
 samtools mpileup -g -f reference.fna aligned.bam > variants.bcf #Variant calling
