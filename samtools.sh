6054a4d43d9b3bdf4fb1377e484045256e0e0ffc
Nathan Wong <nathan2wong@gmail.com>
2016-02-18 14:27:35 -0800

    fixed auto script

--- samtools.sh
+++ samtools.sh
@@ -3,11 +3,11 @@
 #Requires input of "aligned.sam" and "reference.fna"
 ./samtools view -b -S -o aligned.bam aligned.sam #Converting SAM to BAM
 echo "111"
-./samtools sort aligned.bam aligned.sorted #Sort BAM file
+./samtools sort -T aligned.sorted -o aligned.sorted.bam aligned.bam #Sort BAM file
 #Variant calling 
 echo "222"
 ./samtools mpileup -g -f reference.fna aligned.sorted.bam > variants.bcf & 
-./bcftools call -c -v variants.bcf > variants.vcf & #BCF to VCF File 
+./bcftools call -c -v variants.bcf > variants.vcf #BCF to VCF File 
 ./samtools mpileup -f reference.fna aligned.sorted.bam | java -jar VarScan.v2.3.7.jar mpileup2snp > variants2.vcf #Varscan
 
 echo "333"
