4355ee442886d0f38e0ceab414365ec97773dce6
Nathan Wong <nathan2wong@gmail.com>
2016-02-17 18:53:44 -0800

    fixed parallelization issue

--- samtools.sh
+++ samtools.sh
@@ -6,10 +6,11 @@ echo "111"
 ./samtools sort aligned.bam aligned.sorted #Sort BAM file
 #Variant calling 
 echo "222"
-./samtools mpileup -g -f reference.fna aligned.bam > variants.bcf & 
-java -jar VarScan.v2.3.7.jar mpileup2snp -g -f reference.fna varscandata/genomes/sim_reads_aligned.sorted.bam > sim_variants.bcf
+./samtools mpileup -g -f reference.fna aligned.sorted.bam > variants.bcf & 
+./bcftools call -c -v variants.bcf > variants.vcf & #BCF to VCF File 
+./samtools mpileup -f reference.fna aligned.sorted.bam | java -jar VarScan.v2.3.7.jar mpileup2snp > variants2.vcf #Varscan
 
 echo "333"
 
-#bcftools call -c -v variants.bcf > variants.vcf #BCF to VCF File
+
 #./samtools tview variants.vcf #view output
