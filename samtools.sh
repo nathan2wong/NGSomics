a4ab95753be132101207c485f9a715e38c2949ff
Nathan Wong <nathan2wong@gmail.com>
2016-02-18 16:18:44 -0800

    fixed error + removed ./

--- samtools.sh
+++ samtools.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #Requires input of "aligned.sam" and "reference.fna"
-./samtools view -b -S -o aligned.bam aligned.sam #Converting SAM to BAM
+samtools view -b -S -o aligned.bam aligned.sam #Converting SAM to BAM
 echo "111"
-./samtools sort -T aligned.sorted -o aligned.sorted.bam aligned.bam #Sort BAM file
+samtools sort -T aligned.sorted -o aligned.sorted.bam aligned.bam #Sort BAM file
 #Variant calling 
 echo "222"
-./samtools mpileup -g -f reference.fna aligned.sorted.bam > variants.bcf & 
-./bcftools call -c -v variants.bcf > variants.vcf #BCF to VCF File 
-./samtools mpileup -f reference.fna aligned.sorted.bam | java -jar VarScan.v2.3.7.jar mpileup2snp > variants2.vcf #Varscan
+samtools mpileup -g -f reference.fna aligned.sorted.bam > variants.bcf && bcftools view -c -v variants.bcf > variants.vcf #BCF to VCF File 
+samtools mpileup -f reference.fna aligned.sorted.bam | java -jar VarScan.v2.3.7.jar mpileup2snp > variants2.vcf #Varscan
 
 echo "333"
 
