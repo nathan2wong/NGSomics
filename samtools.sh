58cd89a11e0034b3d103ed135ac79f950c374a9c
Nathan <nathan2wong@gmail.com>
2016-02-27 17:05:53 -0800

    freebayes implement

--- samtools.sh
+++ samtools.sh
@@ -6,9 +6,11 @@ echo "111"
 samtools sort -T aligned.sorted -o aligned.sorted.bam aligned.bam #Sort BAM file
 #Variant calling 
 echo "222"
-samtools mpileup -g -f reference.fna aligned.sorted.bam > variants.bcf && bcftools view -c -v variants.bcf > variants.vcf #BCF to VCF File 
+samtools mpileup -g -f reference.fna aligned.sorted.bam > variants.bcf
+echo "test"
+bcftools view variants.bcf > variants.vcf #BCF to VCF File 
 samtools mpileup -f reference.fna aligned.sorted.bam | java -jar VarScan.v2.3.7.jar mpileup2snp > variants2.vcf #Varscan
-
+freebayes -f reference.fna aligned.sorted.bam > variants3.vcf #Freebayes
 echo "333"
 
 
