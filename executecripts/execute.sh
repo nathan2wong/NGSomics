8aa4a5cdaf04b02e5d091439867b79221e6db52e
Nathan <nathan2wong@gmail.com>
2016-03-06 15:01:40 -0800

    execution script

---
+++ executecripts/execute.sh
@@ -0,0 +1,27 @@
+while true; do
+                read -p "Do you wish to begin NGSomics on trial 1?" yn
+                        case $yn in
+                        [Yy]* ) sh script.sh; break;;
+                        [Nn]* ) exit;;
+                        *) echo "Please answer yes or no.";;
+                esac
+        done
+\
+while true; do
+                read -p "Do you wish to remove excess data?" yn
+                        case $yn in
+                        [Yy]* )
+                            rm aligned.bam aligned.sorted.bam out.log reference.fna.fai         variants.bcf variants.vcf variants2.vcf variants3.vcf;
+                    while true; do
+                        read -p "Do you wish to view the results now?" yn
+                            case $yn in 
+                            [Yy]* )
+                            cat "out.diff.sites_in_files"; break;;
+                            [Nn]* ) echo "Done!"; ls; exit;;
+                            *) echo "Please answer yes or no.";;
+                        esac
+                    done
+                        [Nn]* ) echo "Done!"; exit;;
+                        *) echo "Please answer yes or no.";;
+                esac
+        done
